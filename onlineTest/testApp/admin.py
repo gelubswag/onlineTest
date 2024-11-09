ff380d2991ff16e1eb3ec4656cf57bb8a4cd984f
gelubswag <doshdik35@gmail.com>
2024-11-09 19:29:17 +0300

    Delete onlineTest directory

--- onlineTest/testApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from testApp import models
-admin.site.register(models.Subject)
-admin.site.register(models.SubjTest)
-admin.site.register(models.Question)
-
-# Register your models here.
--- onlineTest/testApp/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from testApp import models
-
-class SubjectForm(ModelForm):
-    
-    class Meta:
-        model = models.Subject
-        fields = ["name"]
