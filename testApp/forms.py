8c5040ae3fb5696f54aeb70274017dc3b7b129fa
gelubswag <doshdik35@gmail.com>
2024-11-30 00:13:39 +0300

    Add files via upload

--- testApp/forms.py
+++ testApp/forms.py
@@ -1,31 +1,39 @@
 from django.forms import ModelForm, Textarea
 from testApp import models
 
+
 class SubjectForm(ModelForm):
-    
+
     class Meta:
         model = models.Subject
         fields = ["name"]
 
+
 class TestForm(ModelForm):
-    
+
     class Meta:
         model = models.SubjTest
-        fields = ["name","questions_num"]
-        
+        fields = ["name", "questions_num", "test_questions_num"]
+
+
 class QuesttForm(ModelForm):
-    
+
     class Meta:
         model = models.Question
-        fields = ["image","imageURL","text","optionsNum","weight"]
-        widgets = {'vision': Textarea(attrs={'rows':6,
-                                            'cols':22,
-                                            'style':'resize:none',
-                                            }),
+        fields = ["image", "imageURL", "text", "optionsNum", "weight"]
+        widgets = {
+            "vision": Textarea(
+                attrs={
+                    "rows": 6,
+                    "cols": 22,
+                    "style": "resize:none",
+                }
+            ),
         }
 
+
 class OptForm(ModelForm):
-    
+
     class Meta:
         model = models.Option
-        fields = ["image","imageURL","text","isRight"]
+        fields = ["image", "imageURL", "text", "isRight"]
